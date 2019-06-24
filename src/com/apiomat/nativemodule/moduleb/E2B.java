2097a3325635b6c254adf5f8be64a8b31b08d2aa
nobody <nobody@yambas-apiomat-yambas-6f6bbb7c79-4gfr4>
2019-06-24 16:51:51 +0200

    [AOM] Change code for module ModuleB in system TEST

---
+++ src/com/apiomat/nativemodule/moduleb/E2B.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2011 - 2019, Apinauten GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.apiomat.nativemodule.moduleb;
+
+
+import com.apiomat.nativemodule.basics.*;
+/**
+* Generated class for your E2B data model
+*
+* DO NOT CHANGE ANY CODE EXCEPT CLASS ANNOTATIONS OR CLASS ATTRIBUTES HERE!
+* EVERYTHING ELSE WILL GET OVERWRITTEN!
+*
+*/
+@java.lang.SuppressWarnings( "unused" )
+@com.apiomat.nativemodule.Model( moduleName = "ModuleB",
+    hooksClassNameTransient = "com.apiomat.nativemodule.moduleb.E2BHooksTransient", 
+    hooksClassNameNonTransient = "com.apiomat.nativemodule.moduleb.E2BHooksNonTransient", 
+    isTransient = false,     requiredUserRoleCreate=com.apiomat.nativemodule.UserRole.User, requiredUserRoleRead=com.apiomat.nativemodule.UserRole.User,
+    requiredUserRoleWrite=com.apiomat.nativemodule.UserRole.Owner, restrictResourceAccess=false,    allowedRolesCreate={}, allowedRolesRead={},
+    allowedRolesWrite={}, allowedRolesGrant={}, 
+    roleClassesMap={})
+public class E2B extends com.apiomat.nativemodule.AbstractClientDataModel<com.apiomat.nativemodule.moduleb.E2B> implements com.apiomat.nativemodule.IModel<com.apiomat.nativemodule.moduleb.E2B>
+{
+    /**
+     * Contains the name of the module that this model belongs to
+     */
+    public static final String MODULE_NAME = "ModuleB";
+    /**
+     * Contains the name of the model
+     */
+    public static final String MODEL_NAME = "E2B";
+
+    /** class specific attributes */
+    /**
+     * Protected constructor; to create a new instance, use the createObject() method
+     */
+    public E2B ()
+    {}
+
+    /**
+     * Returns the name of the module where this class belongs to
+     */
+    @Override
+    public String getModuleName( )
+    {
+        return MODULE_NAME;
+    }
+
+    /**
+     * Returns the name of the model
+     */
+    @Override
+    public String getModelName( )
+    {
+        return MODEL_NAME;
+    }
+
+}
